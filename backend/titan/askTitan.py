81fadaeb6001990d95de81f0019c307710dbce6b
Pawel Krzywicki <pwlkrz@amazon.com>
2023-08-15 02:22:45 +0200

    ConversationBufferWindowMemory

--- backend/titan/askTitan.py
+++ backend/titan/askTitan.py
@@ -3,40 +3,48 @@ import os
 
 import boto3
 
+from ddbSession import ChatMessagesSession
+
 from langchain.embeddings import BedrockEmbeddings
 from langchain.vectorstores import FAISS
 
-from ddbSession import ChatSession
+from langchain.llms.bedrock import Bedrock
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.vectorstores.base import VectorStoreRetriever
 
 is_cold_start = True
 bedrock = None
 vectorstores = []
 session = None
+modelId = "amazon.titan-tg1-large"
+textGenerationConfig = {
+    "maxTokenCount": 4096,
+    "stopSequences": [],
+    "temperature": 0.5,
+    "topP": 0.2,
+}
 
-def call_bedrock(bedrock, prompt):
+
+def call_bedrock(bedrock_client, prompt):
     prompt_config = {
         "inputText": prompt,
-        "textGenerationConfig": {
-            "maxTokenCount": 4096,
-            "stopSequences": [],
-            "temperature": 0.5,
-            "topP": 0.2,
-        },
+        "textGenerationConfig": textGenerationConfig
     }
 
     body = json.dumps(prompt_config)
 
-    modelId = "amazon.titan-tg1-large"
     accept = "application/json"
-    contentType = "application/json"
+    content_type = "application/json"
 
-    response = bedrock.invoke_model(
-        body=body, modelId=modelId, accept=accept, contentType=contentType
+    response = bedrock_client.invoke_model(
+        body=body, modelId=modelId, accept=accept, contentType=content_type
     )
     response_body = json.loads(response.get("body").read())
 
     results = response_body.get("results")[0].get("outputText")
     return results
+
 
 def connectToBedrock():
     roleArn = os.environ["titanRoleArn"]
@@ -53,13 +61,16 @@ def connectToBedrock():
                         #endpoint_url="https://bedrock.us-east-1.amazonaws.com",
                         )
 
+
 def getDocs(query, vectorstore, k=4):
-    docs =  vectorstore.similarity_search(query, k=k)
+    docs = vectorstore.similarity_search(query, k=k)
     return docs
+
 
 def lambda_handler(event, context):
     log_questions = False
-    if not "query" in event:
+    body = {}
+    if "query" not in event:
         if 'body' not in event:
             return {
                 'statusCode': 400,
@@ -81,17 +92,17 @@ def lambda_handler(event, context):
 
     if is_cold_start:
         bedrock = connectToBedrock()
-        indexes = ["/opt/index_faiss", "/opt/index_faiss_3"]
+        indexes = ["/opt/index_faiss"]
         embeddings = BedrockEmbeddings(client=bedrock)
         vectorstores = [FAISS.load_local(index, embeddings) for index in indexes]
-        session = ChatSession()
+        session = ChatMessagesSession()
         is_cold_start = False
 
     session.init(event)
-    #print(f"Session: {session.sess_id}, {session.data}")
-    #queries = query.split("_")
+    msgHistory = session.load()
+    llm = Bedrock(client=bedrock, model_id=modelId, model_kwargs=textGenerationConfig)
 
-    if body['contextReturnNumber'] == "":
+    if 'contextReturnNumber' not in body or body['contextReturnNumber'] == "":
         body['contextReturnNumber'] = 4
     else:
         try:
@@ -99,46 +110,53 @@ def lambda_handler(event, context):
         except:
             body['contextReturnNumber'] = 4
 
-    if body['promptSuffix'] == "":
+    if 'promptSuffix' not in body or body['promptSuffix'] == "":
         body['promptSuffix'] = f"""Question: {query}
             Answer:"""
 
-    if body['contextQuestions'] == "":
+    if 'contextQuestions' not in body or body['contextQuestions'] == "":
         contextQuestions = [query]
     else:
         contextQuestions = body['contextQuestions'].split('_')
 
     answers = []
-    for idx in range(len(vectorstores)):
+    for vectorstore in vectorstores:
         # Find docs
         context = ""
         doc_sources_string = []
 
         for q in contextQuestions:
-            docs = getDocs(q, vectorstores[idx], k=body['contextReturnNumber'])
+            docs = getDocs(q, vectorstore, k=body['contextReturnNumber'])
             for doc in docs:
                 doc_sources_string.append(doc.metadata)
                 context += doc.page_content
 
-        if body['context'] != "":
+        if 'context' in body and body['context'] != "":
             context = body["context"]
 
-        prompt = f"""{body['promptPrefix']}
+        prompt = f"""{body['promptPrefix'] if 'promptPrefix' in body else ''}
     
         {context}
         
         {body['promptSuffix']}
         """
 
+        # first - use just a prompt
         generated_text = call_bedrock(bedrock, prompt)
         answers.append({"answer": str(generated_text), "docs": doc_sources_string, "context": context, "prompt": prompt})
+
+    # use stored messages
+    memory = ConversationBufferWindowMemory(chat_memory=msgHistory, memory_key="chat_history", return_messages=True, human_prefix="Human:", ai_prefix="Bot:")
+    retriever = VectorStoreRetriever(vectorstore=vectorstores[0])
+    conversation_with_retrieval = ConversationalRetrievalChain.from_llm(llm, retriever, memory=memory)
+    chat_response = conversation_with_retrieval({"question": query})
+    answers.append({"answer": chat_response['answer']})
 
     resp_json = {"answers": answers}
     if log_questions:
         print({"question": query, "answers": answers})
 
-    #TODO - put something useful
-    session.put(query, answers[0]["answer"])
+    session.put(query, answers, memory)
     return {
         'statusCode': 200,
         'body': json.dumps(resp_json)
--- backend/titan/ddbSession.py
+++ backend/titan/ddbSession.py
@@ -1,7 +1,11 @@
 import os
-import boto3
+import json
 import time
+import boto3
 import hashlib
+
+from langchain.schema.messages import messages_from_dict, messages_to_dict
+from langchain.memory.chat_message_histories import ChatMessageHistory
 
 KEY = os.getenv('TableKey')
 TABLE = os.getenv('Table')
@@ -9,14 +13,17 @@ TTL_FIELD = os.getenv('TTL')
 TTL_TIME = int(os.getenv('TTLtime'))
 ANSWERS_FIELD = os.getenv('Answers')
 QUESTIONS_FIELD = os.getenv('Questions')
+CHAT_FIELD = os.getenv('Chat')
 
 COOKIE_NAME = os.getenv('Cookie')
+
 
 class DDBsession:
     def get(self):
         if self.sess_id is None:
             return
-        self.item = self._ddb.get_item(TableName=TABLE, Key={KEY: {"S": self.sess_id}}).get("Item", {})
+        self.item = self._ddb.get_item(TableName=TABLE, Key={KEY: {"S": self.sess_id}},
+                                       ProjectionExpression=self.projection_expression).get("Item", {})
 
     def put(self):
         if self.sess_id is None:
@@ -37,17 +44,21 @@ class DDBsession:
 
     def __init__(self):
         self.sess_id = None
+        self.item = None
         self._ddb = boto3.client(service_name="dynamodb")
+        # set projection expression
+        self.projection_expression = KEY
         # set update expression
         self.update_expression = 'ADD #counter :increment SET #createdAt = if_not_exists(#createdAt, :now), #ttl = :ttl'
-        self.expression_attribute_names={
+        self.expression_attribute_names = {
             '#counter': 'AccessCounter',
             '#createdAt': 'CreatedAt',
             '#ttl': TTL_FIELD
         }
-        self.expression_attribute_values={
+        self.expression_attribute_values = {
             ':increment': {"N": str(1)}
         }
+
 
 # find session id
 # get JWT token and use the signature as a session id
@@ -57,22 +68,24 @@ def get_session_key(event):
     for cookie_set in event['cookies']:
         for cookie in cookie_set.split(";"):
             tokens = cookie.split("=")
-            #print(f"token {tokens}")
-            if COOKIE_NAME != tokens[0].strip() or len(tokens)<2:
+            # print(f"token {tokens}")
+            if COOKIE_NAME != tokens[0].strip() or len(tokens) < 2:
                 continue
             token = "=".join(tokens[1:]).strip()
-            #print(f"pure token {token}")
+            # print(f"pure token {token}")
             parts = token.split(".")
-            if len(parts) != 3 or len(parts[2])<1:
+            if len(parts) != 3 or len(parts[2]) < 1:
                 return None
             m = hashlib.sha256()
             m.update(parts[2].encode("ascii"))
             return m.hexdigest()
 
+
 class DDBsessionJWT(DDBsession):
     def init(self, event):
         self.sess_id = get_session_key(event)
         super().init()
+
 
 class ChatSession(DDBsessionJWT):
     def __init__(self):
@@ -83,7 +96,28 @@ class ChatSession(DDBsessionJWT):
         self.expression_attribute_names["#answers"] = ANSWERS_FIELD
         self.expression_attribute_values[':empty_list'] = {"L": []}
 
-    def put(self, query, answer):
+    def put(self, query, answers):
         self.expression_attribute_values[':query'] = {"L": [{"S": query}]}
-        self.expression_attribute_values[':answer'] = {"L": [{"S": answer}]}
+        self.expression_attribute_values[':answer'] = {"L": [{"SS": [ans["answer"] for ans in answers]}]}
         super().put()
+
+
+class ChatMessagesSession(ChatSession):
+    def __init__(self):
+        super().__init__()
+        self.update_expression += ", #chat = :messages"
+        self.projection_expression += f", {CHAT_FIELD}"
+        self.expression_attribute_names["#chat"] = CHAT_FIELD
+
+    def load(self):
+        chat_state = self.item[CHAT_FIELD]["S"] if CHAT_FIELD in self.item else "{}"
+        return ChatMessageHistory(messages=messages_from_dict(json.loads(chat_state)))
+
+    def get(self):
+        super().get()
+        self.load()
+
+    def put(self, query, answers, memory):
+
+        self.expression_attribute_values[':messages'] = {"S": json.dumps(messages_to_dict(memory.buffer))}
+        super().put(query, answers)
