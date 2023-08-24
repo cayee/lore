07549fc5e0d5a29d2a76abdf1171d957b774adee
Pawel Krzywicki <pwlkrz@amazon.com>
2023-08-24 23:43:25 +0200

    js optimization

--- frontend/ai/lore.js
+++ frontend/ai/lore.js
@@ -29,6 +29,15 @@ function insertMsg(node, msg, chat, user=false) {
     msgNode.scrollIntoView(true);
 }
 
+function postAsk(api, data, callback) {
+    let request = new XMLHttpRequest();
+    request.open('POST', api, true);
+    request.setRequestHeader('Content-type', 'application/json');
+    request.withCredentials = true;
+    request.onload = callback;
+    request.send(JSON.stringify(data));
+}
+
 function ask() {
     const question = userQuery.value;
     if (question.length < 1) return;
@@ -36,37 +45,49 @@ function ask() {
     hourglass.style.display = "inline";
     insertMsg(msgBot, question, chat, true);
 
-    callAsk(question, function() { //TODO pass whether to save the question
-        let answer = `Not ready ${this.status}`;
-        if (this.status === 403 || this.status === 401) {
-            answer = `Not allowed ${this.status}`;
-        } else if (this.status === 200) {
-            let response = JSON.parse(this.response);
-            answer = response.answers[0].answer;
-        }
-        else if (this.status === 429) {
-            answer = "Rate limited";
-        }
-        hourglass.style.display = "none";
-        insertMsg(msgYou, answer, chat);
+    postAsk("AskLore", {"query":question, "logQuestions": logQs.checked,
+        "contextQuestions": cntxtQ.value, "contextReturnNumber": cntxtNumber.value, "promptPrefix": prptPre.value,
+        "context": cntxtField.value, "promptSuffix": prptSuf.value, "sessId": "Lore"},
+        function() { //TODO pass whether to save the question
+            let answer = `Not ready ${this.status}`;
+            if (this.status === 403 || this.status === 401) {
+                answer = `Not allowed ${this.status}`;
+            } else if (this.status === 200) {
+                let response = JSON.parse(this.response);
+                answer = response.answers[0].answer;
+            }
+            else if (this.status === 429) {
+                answer = "Rate limited";
+            }
+            hourglass.style.display = "none";
+            insertMsg(msgYou, answer, chat);
 
-        if (this.status === 403 || this.status === 401) {
-            window.location.href = "index.html";
-        }
+            if (this.status === 403 || this.status === 401) {
+                window.location.href = "index.html";
+            }
     });
 }
 
-function callAsk(msg, callback){
-    let request = new XMLHttpRequest();
-    request.open('POST', "AskLore", true);
-    request.setRequestHeader('Content-type', 'application/json');
-    request.withCredentials = true;
-    request.onload = callback;
-    request.send(JSON.stringify({"query":msg, "logQuestions": logQs.checked,
-        "contextQuestions": cntxtQ.value, "contextReturnNumber": cntxtNumber.value, "promptPrefix": prptPre.value,
-        "context": cntxtField.value, "promptSuffix": prptSuf.value, "sessId": "Lore"}));
+function handleViResponse() {
+    let answer = `Not ready ${this.status}`;
+    let storyLocation = locationVi.textContent.substring(10);   // "Location: ".length
+    if (this.status === 403 || this.status === 401) {
+        answer = `Not allowed ${this.status}`;
+    } else if (this.status === 200) {
+        let response = JSON.parse(this.response);
+        answer = response.answers[0].answer;
+        storyLocation = response.answers[0].location
+    }
+    else if (this.status === 429) {
+        answer = "Rate limited";
+    }
+    hourglassVi.style.display = "none";
+    insertMsg(msgYouVi, answer, chatVi);
+    locationVi.textContent = "Location: " + storyLocation
+    if (this.status === 403 || this.status === 401) {
+        window.location.href = "index.html";
+    }
 }
-
 function ask2() {
     const question = userQueryVi.value;
     if (question.length < 1) return;
@@ -74,68 +95,12 @@ function ask2() {
     hourglassVi.style.display = "inline";
     insertMsg(msgBotVi, question, chatVi);
 
-    callAskVi(question, function() { //TODO pass whether to save the question
-        let answer = `Not ready ${this.status}`;
-        let storyLocation = locationVi.textContent.substring(10);   // "Location: ".length
-        if (this.status === 403 || this.status === 401) {
-            answer = `Not allowed ${this.status}`;
-        } else if (this.status === 200) {
-            let response = JSON.parse(this.response);
-            answer = response.answers[0].answer;
-            storyLocation = response.answers[0].location
-        }
-        else if (this.status === 429) {
-            answer = "Rate limited";
-        }
-        hourglassVi.style.display = "none";
-        insertMsg(msgYouVi, answer, chatVi);
-        locationVi.textContent = "Location: " + storyLocation
-        if (this.status === 403 || this.status === 401) {
-            window.location.href = "index.html";
-        }
-    });
-}
-
-function callAskVi(msg, callback){
-    let request = new XMLHttpRequest();
-    request.open('POST', "AskVi", true);
-    request.setRequestHeader('Content-type', 'application/json');
-    request.withCredentials = true;
-    request.onload = callback;
-    request.send(JSON.stringify({"query":msg, "logQuestions": logQs.checked, "resetChat": false, "sessId": "Vi"}));
+    postAsk("AskVi", {"query": question, "logQuestions": logQs.checked, "resetChat": false, "sessId": "Vi"}, handleViResponse)
 }
 
 function askViAgain() {
     hourglassVi.style.display = "inline";
-    callAskLast("Vi", function() { //TODO pass whether to save the question
-        let answer = `Not ready ${this.status}`;
-        let storyLocation = locationVi.textContent.substring(10);   // "Location: ".length
-        if (this.status === 403 || this.status === 401) {
-            answer = `Not allowed ${this.status}`;
-        } else if (this.status === 200) {
-            let response = JSON.parse(this.response);
-            answer = response.answers[0].answer;
-            storyLocation = response.answers[0].location
-        }
-        else if (this.status === 429) {
-            answer = "Rate limited";
-        }
-        hourglassVi.style.display = "none";
-        insertMsg(msgYouVi, answer, chatVi);
-        locationVi.textContent = "Location: " + storyLocation
-        if (this.status === 403 || this.status === 401) {
-            window.location.href = "index.html";
-        }
-    });
-}
-
-function callAskLast(sessId, callback){
-    let request = new XMLHttpRequest();
-    request.open('POST', "AskLast", true);
-    request.setRequestHeader('Content-type', 'application/json');
-    request.withCredentials = true;
-    request.onload = callback;
-    request.send(JSON.stringify({"sessId": sessId}));
+    postAsk("AskLast", {"sessId": "Vi"}, handleViResponse)
 }
 
 document.getElementById("query").addEventListener("keydown", function(e) {
