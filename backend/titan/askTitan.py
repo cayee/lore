053f5a4f89de1a75a9265f1e08c367630ce2a984
Sqatcher <kacper.krzywicki@wp.pl>
2023-09-07 16:30:27 +0200

    Prompt is a bit more pretty

--- backend/titan/askTitan.py
+++ backend/titan/askTitan.py
@@ -82,16 +82,16 @@ def lambda_handler(event, _):
 
     generated_control_ans = ""
     if convSubject != "":
-        generated_control_ans = call_bedrock(bedrock, """This is the conversation between Human and Bot in JSON format: {["conversation": \"""" + body["promptSuffix"][14:] + """\"]}. Does the Human's last question refer to """ + convSubject + "?")
+        generated_control_ans = call_bedrock(bedrock, """This is the conversation between Human and Bot in JSON format: {["conversation": \"""" + body["promptSuffix"][14:] + """\"]}. Does the Human's last question refer to """ + convSubject + "?\n")
         print(generated_control_ans)
         if generated_control_ans[-1] == '.':
             generated_control_ans = generated_control_ans[:-1]
-        if generated_control_ans not in topicList or "no" in generated_control_ans.lower():
+        if (generated_control_ans not in topicList or "no" in generated_control_ans.lower()) and "yes" not in generated_control_ans.lower():
             body['contextQuestions'] = query
             convSubject = ""
     print(generated_control_ans)
     if convSubject == "":
-        msgHistory["location"] = call_bedrock(bedrock, """This is the conversation between Human and Bot in JSON format: {["conversation": \"""" + body["promptSuffix"][14:] + """\"]}. Which characters, regions or events does the question '""" + query + """' refer to? List all the names. Provide answer in a JSON format as follows: {['names': names]}""")
+        msgHistory["location"] = call_bedrock(bedrock, """This is the conversation between Human and Bot in JSON format: {["conversation": \"""" + body["promptSuffix"][14:] + """\"]}. Which characters, regions or events does the question '""" + query + """' refer to? List all the names. Provide answer in a JSON format as follows: {['names': names]}.\n""")
 
     if 'contextQuestions' not in body or body['contextQuestions'] == "":
         contextQuestions = previousUserMessages if len(previousUserMessages) <= 3 else previousUserMessages [-3:]
